9bbf0aea0adf2d9d87fb635f2273f7af1b9a9bfb
Siwanont Sittinam <siwanont1@gmail.com>
2018-09-02 05:07:34 +0700

    add week 3

--- src/pages/week3/index.js
+++ src/pages/week3/index.js
@@ -3,106 +3,376 @@ import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
-import Typography from '@material-ui/core/Typography';
-
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-import Paper from '@material-ui/core/Paper';
+import ReactEcharts from 'echarts-for-react';
 
 const styles = theme => ({
-    root: {
-        width: '100%',
-        overflowX: 'auto',
-    },
-    table: {
-        minWidth: 700,
-    },
     title: {
-        paddingTop: '0.8em',
+        paddingTop: '0.5em',
         flexGrow: 1,
-    },
-    appBarSpacer: theme.mixins.toolbar,
-    content: {
-        flexGrow: 1,
-        padding: theme.spacing.unit * 3,
-        height: '100vh',
-        overflow: 'auto',
-    },
-    chartContainer: {
-        marginLeft: -22,
-    },
-    tableContainer: {
-        height: 320,
     },
     card: {
         minWidth: 275,
-        marginBottom: '0.8em',
+        marginBottom: '1em',
     },
 });
 
-let id = 0;
-function createData(name, calories, fat, carbs, protein) {
-    id += 1;
-    return { id, name, calories, fat, carbs, protein };
+class ChartWk1 extends React.Component {
+
+    getOption1 = () => {
+        return {
+            title: {
+                text: '1. Users in use per hour, day graph',
+            },
+            tooltip: {
+                trigger: 'axis',
+                axisPointer: {
+                    type: 'cross'
+                }
+            },
+            toolbox: {
+                show: true,
+                feature: {
+                    saveAsImage: {}
+                }
+            },
+            xAxis: {
+                type: 'category',
+                boundaryGap: false,
+                data: ['2016/12/20 21:00', '2016/12/31 20:00', '2017/01/01 12:00',
+                    '2017/01/01 13:00', '2017/01/01 14:00', '2017/01/01 15:00',
+                    '2017/01/01 16:00', '2017/01/01 20:00', '2017/01/01 21:00',
+                    '2017/01/01 22:00', '2017/01/01 23:00', '2017/01/02 00:00',
+                    '2017/01/02 01:00', '2017/01/02 02:00', '2017/01/02 03:00',
+                    '2017/01/02 04:00', '2017/01/02 05:00', '2017/01/02 06:00',
+                    '2017/01/02 07:00', '2017/01/02 08:00', '2017/01/02 09:00',
+                    '2017/01/02 10:00', '2017/01/02 11:00', '2017/01/02 12:00',
+                    '2017/01/02 13:00', '2017/01/02 14:00', '2017/01/02 15:00',
+                    '2017/01/02 16:00', '2017/01/02 17:00', '2017/01/02 18:00',
+                    '2017/01/02 19:00', '2017/01/02 20:00', '2017/01/02 21:00',
+                    '2017/01/02 22:00', '2017/01/02 23:00']
+            },
+            yAxis: {
+                type: 'value',
+                axisLabel: {
+                    formatter: '{value} Users'
+                },
+                axisPointer: {
+                    snap: true
+                }
+            },
+            visualMap: {
+                show: false,
+                dimension: 0,
+                pieces: [{
+                    lte: 6,
+                    color: 'green'
+                }, {
+                    gt: 6,
+                    lte: 8,
+                    color: 'red'
+                }, {
+                    gt: 8,
+                    lte: 14,
+                    color: 'green'
+                }, {
+                    gt: 14,
+                    lte: 17,
+                    color: 'red'
+                }, {
+                    gt: 17,
+                    color: 'green'
+                }]
+            },
+            series: [
+                {
+                    name: 'User',
+                    type: 'line',
+                    smooth: true,
+                    data: [1, 1, 1, 1, 2, 1, 3, 3, 1, 6, 44, 144, 74,
+                        38, 40, 26, 48, 115, 178, 368, 432, 426, 462, 539, 416, 524,
+                        410, 388, 325, 417, 476, 399, 394, 272, 162],
+                }
+            ]
+        };
+    };
+
+    getOption2 = () => {
+        return {
+            title: {
+                text: '2. IP usage per hour graph'
+            },
+            tooltip: {
+                trigger: 'axis'
+            },
+            legend: {
+                data: ['IPv4', 'IPv6', 'Dual Stack']
+            },
+            grid: {
+                left: '3%',
+                right: '4%',
+                bottom: '3%',
+                containLabel: true
+            },
+            toolbox: {
+                feature: {
+                    saveAsImage: {}
+                }
+            },
+            xAxis: {
+                type: 'category',
+                boundaryGap: false,
+                data: ['2016/12/20 21:00', '2016/12/31 20:00', '2017/01/01 12:00',
+                    '2017/01/01 13:00', '2017/01/01 14:00', '2017/01/01 15:00',
+                    '2017/01/01 16:00', '2017/01/01 20:00', '2017/01/01 21:00',
+                    '2017/01/01 22:00', '2017/01/01 23:00', '2017/01/02 00:00',
+                    '2017/01/02 01:00', '2017/01/02 02:00', '2017/01/02 03:00',
+                    '2017/01/02 04:00', '2017/01/02 05:00', '2017/01/02 06:00',
+                    '2017/01/02 07:00', '2017/01/02 08:00', '2017/01/02 09:00',
+                    '2017/01/02 10:00', '2017/01/02 11:00', '2017/01/02 12:00',
+                    '2017/01/02 13:00', '2017/01/02 14:00', '2017/01/02 15:00',
+                    '2017/01/02 16:00', '2017/01/02 17:00', '2017/01/02 18:00',
+                    '2017/01/02 19:00', '2017/01/02 20:00', '2017/01/02 21:00',
+                    '2017/01/02 22:00', '2017/01/02 23:00']
+            },
+            yAxis: {
+                type: 'value'
+            },
+            series: [
+                {
+                    name: 'IPv4',
+                    type: 'line',
+                    data: [0, 1, 1, 0, 1, 1, 1, 1, 1, 3, 25, 76, 34, 18, 22, 18, 18, 64, 106, 240, 246, 248, 243, 316, 246, 250, 240, 195, 192, 218, 278, 229, 231, 151, 86]
+                },
+                {
+                    name: 'IPv6',
+                    type: 'line',
+                    stack: '总量',
+                    data: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 0, 0, 0, 0, 4, 0, 0, 0, 2, 0, 0, 0, 0]
+                },
+                {
+                    name: 'Dual Stack',
+                    type: 'line',
+                    stack: '总量',
+                    data: [1, 0, 0, 1, 1, 0, 2, 2, 0, 3, 19, 68, 40, 20, 18, 8, 30, 51, 72, 128, 184, 176, 219, 223, 170, 274, 166, 193, 133, 199, 196, 170, 163, 121, 76]
+                }
+            ]
+        };
+    };
+
+    getOption3 = () => {
+        return {
+            title: {
+                text: '3. IP usage chart'
+            },
+
+            tooltip: {
+                trigger: 'item',
+                formatter: "{a} <br/>{b}: {c} ({d}%)"
+            },
+            legend: {
+                orient: 'vertical',
+                x: 'right',
+                data: ['IPv4', 'IPv6', 'Dual Stack']
+            },
+            series: [
+                {
+                    name: 'IP Usages',
+                    type: 'pie',
+                    radius: ['50%', '70%'],
+                    avoidLabelOverlap: false,
+                    label: {
+                        normal: {
+                            show: false,
+                            position: 'center'
+                        },
+                        emphasis: {
+                            show: true,
+                            textStyle: {
+                                fontSize: '30',
+                                fontWeight: 'bold'
+                            }
+                        }
+                    },
+                    labelLine: {
+                        normal: {
+                            show: false
+                        }
+                    },
+                    data: [
+                        { value: 1320, name: 'IPv4' },
+                        { value: 5, name: 'IPv6' },
+                        { value: 1132, name: 'Dual Stack' }
+                    ]
+                }
+            ]
+        };
+    }
+
+    getOption4 = () => {
+        return {
+            title: {
+                text: '4. Login/Logout Timestamp Graph'
+            },
+            brush: {
+                toolbox: ['rect', 'polygon', 'lineX', 'lineY', 'keep', 'clear'],
+                xAxisIndex: 0
+            },
+            tooltip: {},
+            xAxis: {
+                data: ['2016/12/20 21:00', '2016/12/31 20:00', '2017/01/01 12:00',
+                    '2017/01/01 13:00', '2017/01/01 14:00', '2017/01/01 15:00',
+                    '2017/01/01 16:00', '2017/01/01 20:00', '2017/01/01 21:00',
+                    '2017/01/01 22:00', '2017/01/01 23:00', '2017/01/02 00:00',
+                    '2017/01/02 01:00', '2017/01/02 02:00', '2017/01/02 03:00',
+                    '2017/01/02 04:00', '2017/01/02 05:00', '2017/01/02 06:00',
+                    '2017/01/02 07:00', '2017/01/02 08:00', '2017/01/02 09:00',
+                    '2017/01/02 10:00', '2017/01/02 11:00', '2017/01/02 12:00',
+                    '2017/01/02 13:00', '2017/01/02 14:00', '2017/01/02 15:00',
+                    '2017/01/02 16:00', '2017/01/02 17:00', '2017/01/02 18:00',
+                    '2017/01/02 19:00', '2017/01/02 20:00', '2017/01/02 21:00',
+                    '2017/01/02 22:00', '2017/01/02 23:00'],
+                name: 'X Axis',
+                silent: false,
+                axisLine: { onZero: true },
+                splitLine: { show: false },
+                splitArea: { show: false }
+            },
+            yAxis: {
+                inverse: false,
+                splitArea: { show: false }
+            },
+            grid: {
+                left: 100
+            },
+            series: [
+                {
+                    name: 'login',
+                    type: 'bar',
+                    stack: 'one',
+                    data: [1, 1, 1, 1, 2, 1, 3, 3, 1, 6, 44, 144, 74, 38, 40, 26, 48, 115, 178, 368, 432, 426, 462, 539, 416, 524, 410, 388, 325, 417, 476, 399, 394, 272, 162]
+                },
+                {
+                    name: 'logout',
+                    type: 'bar',
+                    stack: 'one',
+                    data: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -86, -61, -31, -16, -17, -17, -42, -73, -125, -201, -225, -198, -273, -225, -234, -234, -207, -168, -200, -216, -222, -204, -172, -116]
+                }
+            ]
+        };
+    }
+
+    getOption5 = () => {
+        return {
+            title: {
+                text: '5. Agent IP (::ffff:158.108.218.XXX) Distribution Graph'
+            },
+            angleAxis: {
+                type: 'category',
+                data: ['.201', '.202',
+                    '.203', '.204',
+                    '.205', '.206',
+                    '.207', '.208',
+                    '.209', '.210',
+                    '.211', '.212'],
+                z: 700
+            },
+            radiusAxis: {
+            },
+            polar: {
+            },
+            tooltip: {
+                trigger: 'item',
+                formatter: "::ffff:158.108.218{b}: {c} users"
+            },
+            series: [{
+                type: 'bar',
+                data: [644, 643, 553, 665, 570, 570, 589, 567, 548, 592, 523, 673],
+                coordinateSystem: 'polar',
+            }]
+        };
+    }
+
+    getOption6 = () => {
+        return {
+            angleAxis: {
+                type: 'category',
+                data: ['周一', '周二', '周三', '周四', '周五', '周六', '周日'],
+                z: 10
+            },
+            radiusAxis: {
+            },
+            polar: {
+            },
+            series: [{
+                type: 'bar',
+                data: [1, 2, 3, 4, 3, 5, 1],
+                coordinateSystem: 'polar',
+                name: 'A',
+                stack: 'a'
+            }, {
+                type: 'bar',
+                data: [2, 4, 6, 1, 3, 2, 1],
+                coordinateSystem: 'polar',
+                name: 'B',
+                stack: 'a'
+            }, {
+                type: 'bar',
+                data: [1, 2, 3, 4, 1, 2, 5],
+                coordinateSystem: 'polar',
+                name: 'C',
+                stack: 'a'
+            }],
+            legend: {
+                show: true,
+                data: ['A', 'B', 'C']
+            }
+        };
+    }
+
+    render() {
+        const { classes } = this.props;
+        return (
+            <React.Fragment >
+                <Card className={classes.card}>
+                    {/* <Typography variant="headline" component="h2" align="center" color="inherit" noWrap className={classes.title}>
+                        1. Users in use per hour, day graph
+                    </Typography> */}
+                    <CardContent>
+                        <ReactEcharts option={this.getOption1()} style={{ height: '34em', width: '100%' }} />
+                    </CardContent>
+                </Card>
+                <Card className={classes.card}>
+                    <CardContent>
+                        <ReactEcharts option={this.getOption2()} style={{ height: '34em', width: '100%' }} />
+                    </CardContent>
+                </Card>
+                <Card className={classes.card}>
+                    <CardContent>
+                        <ReactEcharts option={this.getOption3()} style={{ height: '34em', width: '100%' }} />
+                    </CardContent>
+                </Card>
+                <Card className={classes.card}>
+                    <CardContent>
+                        <ReactEcharts option={this.getOption4()} style={{ height: '34em', width: '100%' }} />
+                    </CardContent>
+                </Card>
+                <Card className={classes.card}>
+                    <CardContent>
+                        <ReactEcharts option={this.getOption5()} style={{ height: '34em', width: '100%' }} />
+                    </CardContent>
+                </Card>
+                <Card className={classes.card}>
+                    <CardContent>
+                        <ReactEcharts option={this.getOption6()} style={{ height: '34em', width: '100%' }} />
+                    </CardContent>
+                </Card>
+            </React.Fragment >
+        );
+    }
 }
 
-const data = [
-    createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-    createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-    createData('Eclair', 262, 16.0, 24, 6.0),
-    createData('Cupcake', 305, 3.7, 67, 4.3),
-    createData('Gingerbread', 356, 16.0, 49, 3.9),
-];
-
-function SimpleTable(props) {
-    const { classes } = props;
-
-    return (
-        <React.Fragment >
-            <Card className={classes.card}>
-                <Typography variant="headline" component="h2" align="center" color="inherit" noWrap className={classes.title}>
-                    Indo
-                </Typography>
-                <CardContent>
-                    <Paper className={classes.root}>
-                        <Table className={classes.table}>
-                            <TableHead>
-                                <TableRow>
-                                    <TableCell>Dessert (100g serving)</TableCell>
-                                    <TableCell numeric>Calories</TableCell>
-                                    <TableCell numeric>Fat (g)</TableCell>
-                                    <TableCell numeric>Carbs (g)</TableCell>
-                                    <TableCell numeric>Protein (g)</TableCell>
-                                </TableRow>
-                            </TableHead>
-                            <TableBody>
-                                {data.map(n => {
-                                    return (
-                                        <TableRow key={n.id}>
-                                            <TableCell component="th" scope="row">
-                                                {n.name}
-                                            </TableCell>
-                                            <TableCell numeric>{n.calories}</TableCell>
-                                            <TableCell numeric>{n.fat}</TableCell>
-                                            <TableCell numeric>{n.carbs}</TableCell>
-                                            <TableCell numeric>{n.protein}</TableCell>
-                                        </TableRow>
-                                    );
-                                })}
-                            </TableBody>
-                        </Table>
-                    </Paper>
-                </CardContent>
-            </Card>
-        </React.Fragment>
-    );
-}
-
-SimpleTable.propTypes = {
+ChartWk1.propTypes = {
     classes: PropTypes.object.isRequired,
 };
 
-export default withStyles(styles)(SimpleTable);
+export default withStyles(styles)(ChartWk1);
